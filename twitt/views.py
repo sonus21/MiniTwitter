f4f5766d1f11f3dbd8105657a54b2098f1db8090
sonus21 <sonu@job360.io>
2016-04-06 10:52:13 +0530

    Update TODO List

--- twitt/views.py
+++ twitt/views.py
@@ -14,24 +14,27 @@ from twitt.forms import SignUpForm, TwitForm
 
 
 class LoginRequiredMixin(object):
+	"""It ensures that user is authenticated
+
 	"""
-	It ensures that use is authenticated
-	"""
+
 	@method_decorator(login_required)
 	def dispatch(self, request, *args, **kwargs):
 		return super(LoginRequiredMixin, self).dispatch(request, *args, **kwargs)
 
 
 class HomeView(LoginRequiredMixin, TemplateView):
-	"""
-	Home page login is required if not then request
+	"""Home page login is required
+
 	"""
 	template_name = 'index.html'
+
 	def get_context_data(self, **kwargs):
 		conn = redis.StrictRedis(host='localhost', port=6379, db=0)
 
 		# displaying only first 10 twits
-		# Using AJAX it can be used for further twits
+		# TODO adding AJAX request with start and end index
+
 		twit_list = conn.lrange(self.request.user.pk, start=0, end=10)
 
 		twits = []
@@ -39,35 +42,42 @@ class HomeView(LoginRequiredMixin, TemplateView):
 			tp = twit.split('|')
 			tp[1] = tp[1].split('.')[0]
 			twits.append(tp)
-		return {'title': 'Welcome Simple Twitter', 'twits': twits, 'post_form':TwitForm}
+		return {'title': 'Welcome Simple Twitter', 'twits': twits, 'post_form': TwitForm}
 
 
 class FollowUserView(LoginRequiredMixin, CreateView):
+	"""Follow a user
+	1. In get method returns all user list
+	2. In post method follow an user
 	"""
-	Follow a user
-	1. get returns list of users
-	2. post follows a user
-	"""
+
 	def get(self, request, *args, **kwargs):
 		following_list = list(Follow.objects.filter(follower=request.user).values_list('following').values_list('pk',
 		                                                                                                        flat=True))
 		following_list.append(request.user.pk)
+
+		# remove all users that I am following
+
 		users = User.objects.exclude(pk__in=following_list)
-		return TemplateResponse(request, 'follow.html', {'user_list':users})
+		return TemplateResponse(request, 'follow.html', {'user_list': users})
 
 	def post(self, request, *args, **kwargs):
 		user = get_object_or_404(User, pk=request.POST.get('pk'))
 
-		# Direct follow  no notification etc
+		# Direct follow no notification etc
 		# Here AJAX request can be used to optimize the db query
 
 		Follow(follower=request.user, following=user).save()
 
-		#redirect to home page
+		# redirect to home page
 		return redirect('/')
 
 
 class AccountProfileView(TemplateView):
+	"""Display a user profile
+
+	"""
+
 	template_name = 'account_profile.html'
 
 	def get(self, request, *args, **kwargs):
@@ -76,13 +86,11 @@ class AccountProfileView(TemplateView):
 		return TemplateResponse(request, self.template_name, context)
 
 
-
-
 class FollowListView(LoginRequiredMixin, TemplateView):
+	"""List of the user that I am following
+
+	"""
 	template_name = "following.html"
-	"""
-	List of the user that I am following
-	"""
 
 	def get_context_data(self, **kwargs):
 		followings = Follow.objects.filter(follower=self.request.user).values_list('following', flat=True)
@@ -91,53 +99,61 @@ class FollowListView(LoginRequiredMixin, TemplateView):
 
 
 class MyTwitsView(LoginRequiredMixin, TemplateView):
+	"""Display all twits made by me
+
+	"""
 	template_name = 'my_twits.html'
-	"""
-	Display all twits made by me
-	"""
+
 	def get_context_data(self, **kwargs):
-		return {'twit_list':Twit.objects.filter(author=self.request.user)}
+		return {'twit_list': Twit.objects.filter(author=self.request.user)}
 
 
 class SignUpView(FormView):
+	"""Signup a user
+	1. In get return a signup form
+	2. In post method verify user data and create an user
+	"""
 	template_name = 'registration/signup.html'
-	"""
-	Signup a user
-	"""
 
 	def get(self, request, *args, **kwargs):
 		if request.user.is_authenticated():
 			return redirect('/')
 		form = SignUpForm()
-		return TemplateResponse(request, self.template_name, {'form':form})
-
+		return TemplateResponse(request, self.template_name, {'form': form})
 
 	def post(self, request, *args, **kwargs):
 		form = SignUpForm(request.POST)
 		if form.is_valid():
 			form.save()
 			return redirect('login')
-		return TemplateResponse(request, self.template_name, {'form':form})
+		return TemplateResponse(request, self.template_name, {'form': form})
 
 
 class PostTwitView(LoginRequiredMixin, FormView):
-
-	"""
+	"""Post a twit
 	GET method is not allowed so 404
 	"""
+
 	def get(self, request, *args, **kwargs):
 		raise Http404
 
 	def post(self, request, *args, **kwargs):
 		twit_form = TwitForm(request.POST)
 		if twit_form.is_valid():
+
+			# Save twit in main DB
 			twit_form.instance.author = request.user
 			twit_form.save()
 
-			# currently we have use separator as pipe and no partitioning of list is done
+			# Save twit in Redis DB for fast retrieval in the form of list
+			# currently pipe(|) is used as separator
+			# TODO split list if size grows then certain limit
+			# TODO use some other symbol for separator or use escaping technique
+			# TODO If we found that this is taking more than exec a process here and let them do this
+
 			conn = redis.StrictRedis(host='localhost', port=6379, db=0)
 			followings = Follow.objects.filter(follower=request.user).values_list('following', flat=True)
-			msg = "%s|%s|%s" % (request.user, now(),twit_form.cleaned_data['content'])
+			msg = "%s|%s|%s" % (request.user, now(), twit_form.cleaned_data['content'])
 			for fol in followings:
 				conn.lpush(fol, msg)
 		else:
@@ -146,37 +162,43 @@ class PostTwitView(LoginRequiredMixin, FormView):
 
 
 class EditTwitView(LoginRequiredMixin, UpdateView):
-	template_name = 'edit_twit.html'
 	"""
 	Edit a twit
 	"""
+	template_name = 'edit_twit.html'
 
 	def get(self, request, *args, **kwargs):
 		twit = self.get_twit(request, **kwargs)
 		if isinstance(twit, HttpResponse):
 			return twit
 		form = TwitForm(instance=twit)
-		return TemplateResponse(request,self.template_name, {'form': form})
-
+		return TemplateResponse(request, self.template_name, {'form': form})
 
 	def post(self, request, *args, **kwargs):
+
 		twit = self.get_twit(request, **kwargs)
 		if isinstance(twit, HttpResponse):
 			return twit
 
 		form = TwitForm(request.POST, instance=twit)
+
+		# TODO update Redis DB
+
 		if form.is_valid():
 			form.save()
 			return redirect('/')
 
-		return TemplateResponse(request,self.template_name, {'form': form})
+		return TemplateResponse(request, self.template_name, {'form': form})
 
-	def get_twit(self,request, **kwargs):
+	def get_twit(self, request, **kwargs):
+		"""Retrieve a twit from DB and check twit permission
+		if requester has permission to edit twit then return
+		else return to home page(without any error)
+		"""
+
 		pk = kwargs.get('pk')
 		twit = get_object_or_404(Twit, pk=pk)
-		"""
-		User is authenticated and twit belongs to requested user.
-		"""
+
 		if twit.author != request.user:
 			return redirect('/')
 		return twit
@@ -184,8 +206,7 @@ class EditTwitView(LoginRequiredMixin, UpdateView):
 
 def custom_logout(request):
 	"""
-	:param request:
-	:return: redirect to home page
+	Logout an user and redirect to home page
 	"""
 	logout(request)
 	return redirect('/')
